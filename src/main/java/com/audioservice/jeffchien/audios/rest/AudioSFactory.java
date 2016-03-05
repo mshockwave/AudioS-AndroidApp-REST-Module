19ff405b4e5d0ad6510f06095365724bca6714d0
mshockwave <yihshyng223@gmail.com>
2016-03-05 16:50:44 +0800

    Working on application initialization tasks

--- src/main/java/com/audioservice/jeffchien/audios/rest/AudioSFactory.java
+++ src/main/java/com/audioservice/jeffchien/audios/rest/AudioSFactory.java
@@ -8,9 +8,13 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.squareup.okhttp.OkHttpClient;
 
+import java.net.CookieHandler;
 import java.net.CookieManager;
 import java.net.CookiePolicy;
+import java.util.ArrayList;
+import java.util.List;
 
+import retrofit.Converter;
 import retrofit.GsonConverterFactory;
 import retrofit.Retrofit;
 
@@ -21,11 +25,18 @@ public class AudioSFactory {
     private Gson mGson;
     private OkHttpClient mHttpClient;
 
+    private final List<Converter.Factory> mConverters = new ArrayList<>();
+
     public AudioSFactory(){
+        //Default configurations
         mGson = new GsonBuilder()
                 .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
                 .create();
         mHttpClient = new OkHttpClient();
+
+        CookieManager cookieManager = new CookieManager(new PersistentCookieStore(mContext),
+                                        CookiePolicy.ACCEPT_ALL);
+        mHttpClient.setCookieHandler(cookieManager);
     }
 
     public AudioSFactory setContext(Context ctx){
@@ -33,16 +44,38 @@ public class AudioSFactory {
         return this;
     }
 
-    public IAudioS create(){
-        CookieManager cookieManager = new CookieManager(new PersistentCookieStore(mContext),
-                                        CookiePolicy.ACCEPT_ALL);
-        mHttpClient.setCookieHandler(cookieManager);
+    public AudioSFactory setGson(Gson gson){
+        if(gson != null) mGson = gson;
+        return this;
+    }
 
-        Retrofit retrofit = new Retrofit.Builder()
+    public AudioSFactory setHttpClient(OkHttpClient client){
+        if(client != null) mHttpClient = client;
+        return this;
+    }
+    public AudioSFactory setCookieManager(CookieHandler manager){
+        if(manager != null) mHttpClient.setCookieHandler(manager);
+        return this;
+    }
+
+    public AudioSFactory addConvertFactory(Converter.Factory converter){
+        if(converter != null) mConverters.add(converter);
+        return this;
+    }
+
+    public IAudioS create(){
+
+        Retrofit.Builder builder = new Retrofit.Builder()
                         .client(mHttpClient)
                         .baseUrl(Constants.getBaseUriBuilder(false).toString())
-                        .addConverterFactory(GsonConverterFactory.create(mGson))
-                        .build();
-        return retrofit.create(IAudioS.class);
+                        .addConverterFactory(GsonConverterFactory.create(mGson));
+
+        if(mConverters.size() > 0){
+            for(Converter.Factory converter : mConverters){
+                if(converter != null) builder.addConverterFactory(converter);
+            }
+        }
+
+        return builder.build().create(IAudioS.class);
     }
 }
