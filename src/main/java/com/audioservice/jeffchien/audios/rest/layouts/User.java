4548670548fd223047009231784742a05387c141
mshockwave <yihshyng223@gmail.com>
2016-03-07 20:41:10 +0800

    Try to convert layouts to Parcelable

--- src/main/java/com/audioservice/jeffchien/audios/rest/layouts/User.java
+++ src/main/java/com/audioservice/jeffchien/audios/rest/layouts/User.java
@@ -1,7 +1,40 @@
 package com.audioservice.jeffchien.audios.rest.layouts;
 
-public class User {
-    public class Profile {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import java.io.Serializable;
+
+public class User implements Parcelable{
+    protected User(Parcel in) {
+        id = in.readString();
+        email = in.readString();
+        profile = (Profile)in.readSerializable();
+    }
+
+    public static final Creator<User> CREATOR = new Creator<User>() {
+        @Override
+        public User createFromParcel(Parcel in) {
+            return new User(in);
+        }
+
+        @Override
+        public User[] newArray(int size) {
+            return new User[size];
+        }
+    };
+
+    @Override
+    public int describeContents() { return 0; }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(id);
+        dest.writeString(email);
+        dest.writeSerializable(profile);
+    }
+
+    public class Profile implements Serializable{
         public String username;
         public Profile(){}
     }
